3a54e1526b96405668eb78ca5e626ff1911ee5e9
JT <jstorbit@gmail.com>
2025-12-18 17:30:12 -0700

    Refactor Login component to use react-router

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,11 +1,9 @@
 import { NavLink } from "react-router";
 import { useAuth } from "../auth/AuthContext";
-import { usePage } from "./PageContext";
 
 /** Navbar with site navigation links */
 export default function Navbar() {
   const { token, logout } = useAuth();
-  const { setPage } = usePage();
   return (
     <header>
       <p>Fitness Trackr</p>
