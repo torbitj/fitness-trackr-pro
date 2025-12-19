171e7e48b0d4e282d9a3bcc9a7f8646651eea7f1
JT <jstorbit@gmail.com>
2025-12-18 20:00:10 -0700

    Removed unintended add from react

--- src/activities/ActivityDetails.jsx
+++ src/activities/ActivityDetails.jsx
@@ -1,4 +1,4 @@
-import { act, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { getActivityById, deleteActivity } from "../api/activities";
 import { useAuth } from "../auth/AuthContext";
