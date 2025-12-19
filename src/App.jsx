ced33f7497731ce3a901262a43021b7faf99d773
JT <jstorbit@gmail.com>
2025-12-18 17:20:33 -0700

    Refactor App to display using routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { usePage } from "./layout/PageContext";
-
 import Register from "./auth/Register";
 import Login from "./auth/Login";
 import ActivitiesPage from "./activities/ActivitiesPage";
 import Error404 from "./Error404.jsx";
+import Layout from "./layout/Layout.jsx"
+import { Route, Routes } from "react-router";
 
 /**
  * Fitness Trackr is a platform where fitness enthusiasts can share their workouts and
@@ -11,11 +11,12 @@ import Error404 from "./Error404.jsx";
  * account will be able to upload and manage their own activities.
  */
 export default function App() {
-  const { page } = usePage();
-
-  if (page === "register") return <Register />;
-  if (page === "login") return <Login />;
-  if (page === "activities") return <ActivitiesPage />;
-
-  return <Error404 />;
+  <Routes>
+    <Route element={<Layout />}>
+      <Route index element={<ActivitiesPage />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<Error404 />} />
+    </Route>
+  </Routes>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Layout from "./layout/Layout";
 
 import { AuthProvider } from "./auth/AuthContext";
-import { PageProvider } from "./layout/PageContext";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
