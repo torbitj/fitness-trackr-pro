910ca66814a7de96af27903624953c2e58b69d86
JT <jstorbit@gmail.com>
2025-12-20 22:03:41 -0700

    Add RoutinesForm component and error handling

--- src/api/routines.js
+++ src/api/routines.js
@@ -10,3 +10,19 @@ export const getRoutines = async () => {
     return []
   }
 }
+
+export const createRoutine = async (token, routine) => {
+  const response = await fetch(API + "/routines", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: JSON.stringify(routine)
+  })
+
+  if (!response.ok) {
+    const result = await response.json();
+    throw new Error(result.message);
+  }
+}
