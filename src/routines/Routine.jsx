a464c2cb719a1697134108763f66eba127bc643a
JT <jstorbit@gmail.com>
2025-12-22 08:01:52 -0700

    Add delete functionality to delete button to delete routine

--- src/routines/Routine.jsx
+++ src/routines/Routine.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import {useAuth} from "../auth/AuthContext"
-import { useParams } from "react-router";
-import { deleteSet, getRoutineById } from "../api/routines";
+import { useNavigate, useParams } from "react-router";
+import { deleteRoutine, deleteSet, getRoutineById } from "../api/routines";
 import SetForm from "./SetForm";
 
 const Routine = () => {
   const { id } = useParams();
   const { token } = useAuth();
   const [routine, setRoutine] = useState();
+  const [error, setError] = useState();
+  const navigate = useNavigate();
 
   const getRoutine = async () => {
     const routineData = await getRoutineById(id);
@@ -15,7 +17,14 @@ const Routine = () => {
   }
 
   const tryDeleteRoutine = async () => {
+    setError(null);
 
+    try {
+      await deleteRoutine(token, id);
+      navigate("/routines")
+    } catch (e) {
+      setError(e.message)
+    }
   }
 
   useEffect(() => {
@@ -32,7 +41,8 @@ const Routine = () => {
       <p>{routine.goal}</p>
       <h2>Sets</h2>
       {routine.sets.map((set) => <SetItem key={set.id} token={token} set={set} getRoutine={getRoutine} />)}
-      {token && <button>Delete Routine</button>}
+      {token && <button onClick={tryDeleteRoutine}>Delete Routine</button>}
+      {error && <p role="alert">{error}</p>}
       {token && <SetForm routine={routine} getRoutine={getRoutine} />}
     </>
   )
